6d4c79f5bdc88d21838277bb06a34d002efb746b
Felipe Mata <felipe.mata@ucr.ac.cr>
2025-11-26 18:07:49 +0100

    Añadir lo cambiado en solver.c a solver.h, además hacer un poco de documentación de las funciones

--- include/solucionar.h
+++ include/solucionar.h
@@ -6,18 +6,46 @@
 
 /*
  * Construye la matriz Hamiltoniana H (NxN)
- * usando diferencias finitas.
+ * usando diferencias finitas en unidades naturales:
+ *  -H: matriz hamiltoniano (NxN)
+ *  -N: tamaño de la malla
+ *  -xmin, xmax: dominio espacial
+ *  -a: ancho potencial
+ *  -V0: Valor de la grada de potencial
  */
 
 void build_hamiltonian(double **H, int N, double xmin, double xmax,
                        double a, double V0);
 
-/*
- * Resuelve los autovalores del Hamiltoniano usando la biblioteca GSL.
+
+/* Resolver autovalores y autovectores:
+ *  - H: matriz Hamiltoniano (NxN)
+ *  - N: tamaño de la malla
+ *  - valores: arreglo ya reservado de tamaño N para guardar autovalores ordenados asc.
+ *  - vectores: matriz ya creada (N x N). Se guarda como:
+ *        vectores[k][i] = componente i del autovector k
+ *    (es decir, cada fila k contiene la eigenfunción k)
+ *  - xmin, xmax: dominio espacial 
  */
+void solve_eigenpairs(double **H, int N, double *valores, double **vectores,
+                      double xmin, double xmax);
 
+/* Guardar autovalores en archivo:
+ *  - valores: arreglo con N autovalores
+ *  - filename: nombre de archivo a crear
+ *  Devuelve 0 = OK, -1 = error abriendo archivo.
+ */
+int save_eigenvalues(double *valores, int N, const char *filename);
 
-void solve_eigenvalues(double **H, int N);
+/* Guardar la eigenfunción k (fila vectores[k]) en archivo con columnas: x \t psi
+ *  - vectores: matriz con eigenfunciones por fila
+ *  - k: índice del estado a guardar (0..N-1)
+ *  - xmin, xmax: dominio espacial
+ *  - filename: nombre del archivo
+ *  Devuelve 0 = OK, -1 = k fuera de rango, -2 = error abriendo archivo
+ */
+int save_eigenfunction(double **vectores, int k, int N,
+                       double xmin, double xmax, const char *filename);
 
 #endif
 
